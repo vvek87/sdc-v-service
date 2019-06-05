c1ade796b6d6abea8633129a37882d35fc3b43c1
vvek87 <vvek87@gmal.com>
2019-06-05 14:00:35 -0500

    convert books collection into books property in author schema

--- db/genData.js
+++ db/genData.js
@@ -1,23 +1,29 @@
 const faker = require('faker');
 
-const createFakeAuthors = id => ({
-  _id: id,
-  name: faker.name.findName(),
-  followers: faker.random.number(),
-  biography: faker.lorem.paragraph(),
-  author_image: faker.image.people(),
-});
-
-const createFakeBooks = id => ({
-  _id: id,
+const createFakeBooks = () => ({
   title: faker.commerce.productName(),
   year: faker.random.number({ min: 1900, max: 2019 }),
   total_ratings: faker.random.number(),
   average_rating: faker.random.number({ min: 1, max: 5 }),
   description: faker.lorem.paragraph(),
   cover_image: faker.image.city(),
-  author_id: faker.random.number({ min: 1, max: 10000000 }),
 });
+
+const createFakeAuthors = (id) => {
+  const author = {
+    _id: id,
+    name: faker.name.findName(),
+    followers: faker.random.number(),
+    biography: faker.lorem.paragraph(),
+    author_image: faker.image.people(),
+    books: [],
+  };
+
+  for (let i = 1; i <= faker.random.number({ min: 1, max: 5 }); i += 1) {
+    author.books.push(createFakeBooks());
+  }
+  return author;
+};
 
 module.exports = {
   createFakeAuthors,
--- db/index.js
+++ db/index.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 
 const fakeAuthor = require('./genData').createFakeAuthors;
-const fakeBook = require('./genData').createFakeBooks;
 
 mongoose.connect('mongodb://localhost:27017/goodreads', { useNewUrlParser: true });
 const db = mongoose.connection;
@@ -14,37 +13,25 @@ db.once('open', () => {
   console.log('Connected to mongo db');
 });
 
+
 const authorSchema = new mongoose.Schema({
   _id: Number,
   name: String,
   followers: Number,
   biography: String,
   author_image: String,
+  books: Array,
 });
 
-const bookSchema = new mongoose.Schema({
-  _id: Number,
-  title: String,
-  year: Number,
-  total_ratings: Number,
-  average_rating: Number,
-  description: String,
-  cover_image: String,
-  author_id: Number,
-});
 
 const Author = mongoose.model('author', authorSchema);
-const Book = mongoose.model('book', bookSchema);
+
 
 let id = 1;
-let bookId = 1;
-
 let authRecords = [];
-let bookRecords = [];
 
 console.log('Execution timer started...');
 const startTimer = Date.now(); // start timer
-
 
 const seedAuthors = (i) => {
   if (id <= 10000000) {
@@ -62,33 +49,10 @@ const seedAuthors = (i) => {
       console.log('Error inserting records: ', error);
     }
   } else {
-    return;
-  }
-};
-
-seedAuthors(0);
-
-
-const seedBooks = (i) => {
-  if (bookId <= 30000000) {
-    for (let j = 0; j < 100000; j += 1) {
-      bookRecords.push(fakeBook(bookId));
-      bookId += 1;
-    }
-    try {
-      db.collection('books').insertMany(bookRecords)
-        .then(() => {
-          bookRecords = [];
-          seedBooks(i + 1);
-        });
-    } catch (error) {
-      console.log('Error inserting records: ', error);
-    }
-  } else {
     const milliseconds = Date.now() - startTimer; // end timer
     console.log('Execution done after: ', Number.parseFloat(milliseconds / 60000).toFixed(2), ' minutes');
     return;
   }
 };
 
-seedBooks(0);
+seedAuthors(0);
